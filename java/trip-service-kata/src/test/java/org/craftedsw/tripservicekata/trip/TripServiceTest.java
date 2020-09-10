c72ca15957a6e782f657aefcc024d387fc7f2c7f
Thibaut Cantet <thibaut.cantet@gmail.com>
2020-09-10 22:09:18 +0200

    add assertj and dummy unit test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,12 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TripServiceTest {
-	
+    @Test
+    void name() {
+        assertThat(true).isTrue();
+    }
 }
