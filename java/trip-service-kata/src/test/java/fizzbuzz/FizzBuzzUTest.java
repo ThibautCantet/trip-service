ccad09ca3a98d1a33656d7a1ba3018696a5d0a13
Thibaut Cantet <thibaut.cantet@gmail.com>
2021-03-02 17:53:42 +0100

    fizzbuzz

---
+++ java/trip-service-kata/src/test/java/fizzbuzz/FizzBuzzUTest.java
@@ -0,0 +1,13 @@
+package fizzbuzz;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FizzBuzzUTest {
+    @Test
+    void name() {
+        assertThat(true).isTrue();
+    }
+}
